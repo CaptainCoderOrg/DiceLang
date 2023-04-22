ef5552337100c6f24aeb96a1e1928b95ca89f52e
Joseph Collard <jcollard@users.noreply.github.com>
2023-04-22 07:03:08 -0700

    chore: Add IsExternalInit to allow record types.

---
+++ CaptainCoder.DiceLang/IsExternalInit.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("CaptainCoder.Dice.Tests")]
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit { }
+}
